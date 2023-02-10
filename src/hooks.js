6ee12228bb9f35a56217a6e755a8dc135a92aee8
Kevin Templeton <templeton.kevin@gmail.com>
2023-02-10 13:20:05 -0600

    built useAxios custom hook

--- src/hooks.js
+++ src/hooks.js
@@ -1,4 +1,6 @@
 import { useState } from "react";
+import axios from "axios";
+import { v4 as uuid } from "uuid";
 
 const useFlip = (initialFlipState = true) => {
     const [isFlipped, setIsFlipped] = useState(initialFlipState);
@@ -8,4 +10,15 @@ const useFlip = (initialFlipState = true) => {
     return [isFlipped, flip];
 }
 
-export { useFlip };
+const useAxios = (url) => {
+    const [dataArray, setDataArray] = useState([])
+
+    const addData = async () => {
+        const response = await axios.get(url);
+        setDataArray(dataArray => [...dataArray, { ...response.data, id: uuid() }]);
+    };
+
+    return [dataArray, addData];
+}
+
+export { useFlip, useAxios };
